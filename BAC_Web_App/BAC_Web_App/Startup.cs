3caba00fbc0c058f071b4f21f848a58884cd8562
Kehan Meng <pandapanda514@outlook.com>
2020-03-08 16:15:08 -0500

    new changes here

--- BAC_Web_App/BAC_Web_App/Startup.cs
+++ BAC_Web_App/BAC_Web_App/Startup.cs
@@ -13,7 +13,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BAC_Web_App
 {
-    // ew changes here
+    // new changes here
     public class Startup
     {
         public Startup(IConfiguration configuration)
