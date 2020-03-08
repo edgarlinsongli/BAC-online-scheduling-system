16d8c1586f89604408af423347aad25a8411aff8
Kehan Meng <kehan.meng@jacks.sdstate.edu>
2020-03-07 23:51:44 -0600

    Adding existing file

---
+++ BAC_Web_App/BAC_Web_App/Controllers/ABCController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class ABCController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            // The ViewData dictionary object contains data that will be passed to the view.
+            //https://localhost:{PORT}/ABC/Welcome?name=Rick&numtimes=4 -- test
+
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
