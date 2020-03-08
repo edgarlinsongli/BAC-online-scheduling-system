abbe6b4e4356adccc17e0052842c0a305239e4df
Kehan Meng <pandapanda514@outlook.com>
2020-03-08 16:18:57 -0500

    space change

--- BAC_Web_App/BAC_Web_App/Startup.cs
+++ BAC_Web_App/BAC_Web_App/Startup.cs
@@ -13,7 +13,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BAC_Web_App
 {
-    // new changes here
+    // new changes here 
     public class Startup
     {
         public Startup(IConfiguration configuration)
